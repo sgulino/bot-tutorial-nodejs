f2329fa87b84cefcd19fc0964937b7c7d126ee25
Sebastiano V Gulino <sebygulino@gmail.com>
2016-02-17 19:53:27 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]);
   var botRegex = /[Dd][Ii][Nn][Oo][Ss][Aa][Uu][Rr][Ss]?/;
   var botRegexH = /[Ff][Aa][Gg][Gg][Oo][Tt]/;
+  var botRegexFP = /[Ff][Uu][Cc][Kk][ ][Yy][Oo][Uu]/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -16,7 +17,11 @@ function respond() {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
-  } {
+  } else if(request.text && botRegexFP.test(request.text)) {
+    this.res.writeHead(200);
+    postFuckPoland();
+    this.res.end();
+  } else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
@@ -65,6 +70,41 @@ function postMessage() {
   var botResponse, options, body, botReq;
 
   botResponse = "Homophobia will not be tolerated, you flat-footed cunt.";
+  
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postDino() {
+  var botResponse, options, body, botReq;
+  
+  botResponse = "and also, fuck poland";
   
   options = {
     hostname: 'api.groupme.com',
